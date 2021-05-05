c71b669177f28bdfc19fc89aa6d8fd9c8dfd541c
Restiii <restisanchez99@gmail.com>
2021-05-05 13:15:02 +0200

    Update back.sh

--- back.sh
+++ back.sh
@@ -1,51 +1,39 @@
-#! /bin/bash
+#!/bin/bash
+
+# Definimos la contraseña de root como variable
+DB_ROOT_PASSWD=root
+DB_USU_PASSWD=usuario
+
+
+# Instalación y configuración de MySQL
+
+# Habilitamos el modo de shell para mostrar los comandos que se ejecutan
 set -x
-
-# Actualizamos la lista de paquetes
+# Actualizamos y actualizamos la lista de paquetes
 apt update
-
-# Instalamos el MySQL Server
+apt upgrade -y
+# Instalamos el sistema gestor de base de datos
 apt install mysql-server -y
 
-# Instalamos las librerias PHP para MySQL
-apt install debconf-utils -y
+# Copiamos el archivo de configuración de MySQL copiando uno previamente ya modificado mysqld.cnf 
+# mv mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
-# Declaración de variables
-
-IP_PRIVADA_FRONT=3.221.150.99 # Indica la interfaz de red del servidor de MySQL que permite conexiones
-BD_ROOT_PASSWD=root 
-
-# Actualizamos la contraseña de root de MySQL
-mysql -u root <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '$BD_ROOT_PASSWD'; "
-
-# le damos permisos
-mysql -u root  <<< " FLUSH PRIVILEGES ; "
-
-#############################################################
-# Ejecutamos el script de base de datos de la aplicación web#
-#############################################################
-
+# Editamos el archivo de configuración de MySQL, modificando la línea 
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf 
+# Reiniciamos el servicio
+sudo /etc/init.d/mysql restart
 # Clonamos el repositorio
 cd /home/ubuntu
-rm -rf iaw-practica-lamp
+rm -rf iaw-practica-lamp 
 git clone https://github.com/josejuansanchez/iaw-practica-lamp
-mv /var/www/html/iaw-practica-lamp/src/* /var/www/html/
+# Actualizamos la contraseña de root de MySQL
+mysql -u root <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '$DB_ROOT_PASSWD';"
+mysql -u root -p$DB_ROOT_PASSWD <<< "FLUSH PRIVILEGES;"
 
+# Creamos usuario para la aplicación web y asignamos privilegios
+# mysql -u root -p$DB_ROOT_PASSWD <<< "CREATE USER 'lamp_user'@'%' IDENTIFIED BY '$DB_USU_PASSWD';"
+# mysql -u root -p$DB_ROOT_PASSWD <<< "GRANT ALL PRIVILEGES ON 'lamp_db'.* TO 'lamp_user'@'%';"
+# mysql -u root -p$DB_ROOT_PASSWD <<< "FLUSH PRIVILEGES;"
 
-# Eliminamos contenido que no sea útil
-rm -rf /var/www/html/index.html
-rm -rf /var/www/html/iaw-practica-lamp
-
-
-# Importamos el script de creación de la base de datos
-mysql -u root -p$BD_ROOT_PASSWD < /home/ubuntu/iaw-practica-lamp/db/database.sql
-
-#---------------------------------------------------------------#
-#Configuramos MySQL para permitir conexiones desde la IP privada
-#---------------------------------------------------------------#
-sudo sed -i "s/127.0.0.1/$IP_PRIVADA_FRONT/" /etc/mysql/mysql.conf.d/mysqld.cnf
-
-#Reiniciamos servicio MySQL
-systemctl restart mysql
-
-#---------------------------------------------------------------#
+# Introducimos la base de tados
+mysql -u root -p$DB_ROOT_PASSWD < /home/ubuntu/iaw-practica-lamp/db/database.sql
